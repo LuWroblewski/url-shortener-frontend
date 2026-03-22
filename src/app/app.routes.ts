62a10bd9baed39535825ba320ed90d5377dcc2c4
LuWroblewski <wrolucas@gmail.com>
2026-03-22 11:35:10 -0300

    angular config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LoginLayout } from './core/layouts/login-layout/login-layout';
+import { MainLayout } from './core/layouts/main-layout/main-layout';
+import { Dashboard } from './features/dashboard/dashboard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LoginLayout,
+  },
+  {
+    path: 'menu',
+    component: MainLayout,
+    children: [{ path: 'dashboard', component: Dashboard }],
+  },
+  { path: '**', redirectTo: '' },
+];
